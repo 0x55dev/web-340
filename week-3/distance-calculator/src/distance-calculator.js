4db714842db6f8e12677ba7ad83ce0a040a6d58a
Alex Renard <prioxisfx@gmail.com>
2024-04-07 22:56:35 -0500

    completed week 3 assignment

--- week-3/distance-calculator/src/distance-calculator.js
+++ week-3/distance-calculator/src/distance-calculator.js
@@ -1,5 +1,44 @@
+"use strict";
+
 function calculateDistance(planet1, planet2) {
   // TODO: Implement this function
+
+  // so the email stated that we may need to multiply by -1 in order to get a positive number
+  // but depending on the order of the planets we might not need to, if the subtraction is a positive number
+  // then we can just return the distance seen below
+
+  planet1 = planet1;
+  planet2 = planet2;
+  
+  if (!(planet1 in planetDistances) || !(planet2 in planetDistances)) {
+    throw new Error('Invalid planet name(s) provided');
+  }
+  const distance1 = planetDistances[planet1];
+  const distance2 = planetDistances[planet2];
+
+  const distance = distance1 - distance2;
+
+  if(distance < 0) {
+    return -1*(distance1 - distance2); // multiply by -1 in order to get the positive number. Just flipping the sign.
+  } else {
+    return distance;
+  }
 }
 
+// so I think the idea is to export both of these, the function and the object.
+// we can use the object as a parameter instead of having to do string comparisons or lookups within the test.
+// the idea of TDD is to keep things as simple as possible and I think this is the overall way of achieving that.
+
+const planetDistances = {
+  Earth: 1,
+  Mars: 1.52,
+  Mercury: 0.39,
+  Venus: 0.72,
+  Saturn: 9.54,
+  Uranus: 19.22,
+  Neptune: 30.06,
+  Jupiter: 5.20
+}
+
+// export just the function 
 module.exports = calculateDistance;
--- week-3/distance-calculator/test/distance-calculator.spec.js
+++ week-3/distance-calculator/test/distance-calculator.spec.js
@@ -1,8 +1,50 @@
-const assert = require('assert');
-const calculateDistance = require('../src/distance-calculator');
+"use strict";
 
-function testFunctionDescription() {
-  // TODO: Implement this function
+const assert = require('assert');
+const calculateDistance  = require('../src/distance-calculator');
+
+// I removed the description test as I am pretty sure that was just a placeholder?
+// the comment below said to put the test functions so I put them all there.
+
+function testEarthToJupiter() {
+  try { // try catch block to handle errors
+    assert.strictEqual(calculateDistance("Earth", "Jupiter"), 4.20); // we want to make sure the value returned matches what we expect.
+    console.log("Passed!"); // then we return passed.
+    return true;
+  } catch(error) {
+    console.error(`Failed testEarthToJupiter:${error.message}`); 
+    return false;
+  }
+}
+
+function testJupiterToUranus() { // we want to make sure that if we flip the order of the numbers that it still works.
+  try {
+    assert.strictEqual(calculateDistance("Jupiter", "Uranus"), 14.02); 
+    console.log("Passed!");
+    return true;
+  } catch(error) {
+    console.error(`Failed testEarthToJupiter:${error.message}`);
+    return false;
+  }
+}
+
+function testInvalidName() { // we want to make sure that if we flip the order of the numbers that it still works.
+  try {
+    assert.throws(() => { // we want to use assert.throws to make sure that we get a message and the message is what we expect.
+      calculateDistance("Jupite", "Uranus");
+    }, {
+      name: 'Error', // because we use throw new Error 
+      message: 'Invalid planet name(s) provided'
+    });
+    return true;
+  } catch(error) {
+    console.error(`Failed testEarthToJupiter:${error.message}`);
+    return false;
+  }
 }
 
 // Call your test functions here
+
+testJupiterToUranus();
+testEarthToJupiter();
+testInvalidName();
