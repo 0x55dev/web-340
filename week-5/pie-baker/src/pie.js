83201d29418641274b63c3edaeb7ff8372d4b954
Alex Renard <prioxisfx@gmail.com>
2024-04-20 23:14:38 -0500

    Did week 5 assignment

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,34 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: William Renard
+ * Date: 4/20/2024
+ * File Name: pie.js
+ * Description: main file for pie-baker.
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
-  // Your code here
+  const essentialIngredients = ["flour", "sugar", "butter"];
+  // takes a type of pie
+  // takes an array of ingredients
+
+  for(let i = 0; i < ingredients.length; i++) { // loop through all essential ingredients
+    if(!essentialIngredients.includes(ingredients[i])) { // check if the ingredients passed are included.
+      console.log("Warning: Error missing essential ingredient!"); 
+      process.exit(1); // exit
+    }
+  }
+  if(pieType.trim() === "") { // trim the string to hopefully catch an empty white spaces.
+    console.log("Warning: Error missing pie type!"); // if the string is empty then we are missing a pieType
+    process.exit(1); // log and exit
+  }
+  console.log(`Successfully baked ${pieType} pie!`) // otherwise log a successful bake
+  // determine if the pie was successfully baked or not
+  // check if an essential ingredient is missing (flour, sugar, butter)
+
 }
 
 module.exports = { bakePie };
+
+// test 1, check if essential ingredient is missing or not
+// test 2, check if pie was baked successfully or not
+// test 3, check if process.exit is called (make sure this is last)
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,12 +1,47 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: William Renard
+ * Date: 4/20/2024
+ * File Name: pie.spec.js
+ * Description: jest test suite.
  */
 
 "use strict";
 
 const { bakePie } = require("../src/pie");
 
+const exit = jest.spyOn(process, 'exit').mockImplementation((code) => code); // include this line from textbook otherwise tests won't pass despite being correct
+
 // Your tests here
+describe("bakePie function", () => {
+  let log; // this is a holding object for whatever the console message logs from the actual pie.js file.
+
+  beforeEach(() => {
+    log = jest.spyOn(console, "log"); // gives jest control over how Console.Log works
+  });
+
+  afterEach(() => {
+    log.mockRestore(); // restores functionality to normal JavaScript functionality for console.log
+  });
+  // check if passing all essential ingredients will be successful
+  test("Passes all essential ingredients", () => {
+    bakePie("apple", ["flour","sugar","butter"]); // run the bakePie function with all essential ingredients
+    expect(log).toHaveBeenCalledWith("Successfully baked apple pie!"); // expect it to be successful.
+  });
+
+    // check if leaving out pieType will give an error
+  test("Passes all essential ingredients", () => {
+    bakePie("", ["flour","sugar","butter"]); // run the bakePie function with all essential ingredients
+    expect(log).toHaveBeenCalledWith("Warning: Error missing pie type!"); // expect it to be successful.
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+
+  
+  // test 3, check if leaving out an essential ingredient will give an error.
+  test("Leaves out one essential ingredient", () => {
+    bakePie("apple", ["flour","sugar","eggs"]);
+    expect(log).toHaveBeenCalledWith("Warning: Error missing essential ingredient!");
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+})
+
+
