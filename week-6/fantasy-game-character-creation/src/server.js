6ae5b6cf52efb2c721181e1ca78b851744945c00
Alex Renard <prioxisfx@gmail.com>
2024-04-26 22:57:55 -0500

    Did week 6 assignment

--- week-6/fantasy-game-character-creation/src/server.js
+++ week-6/fantasy-game-character-creation/src/server.js
@@ -1,10 +1,56 @@
+/**
+ * Author: William Renard
+ * Date: 4/14/2024
+ * File Name: server.js
+ * Description: the file that node.js will start from.
+ */
+
+
 const http = require('http');
+const { parse } = require('path');
 const url = require('url');
 
-// TODO: Implement your server here
+// because it seems that we need to have only the specific types I am going to create a class that will hold this
+class Character {
+  constructor(role, gender, funFact) {
+    this.role = role;
+    this.gender = gender;
+    this.funFact = funFact;
+  }
+}
 
+let fantasyCharacter = new Character("Warrior", "Male", "A brave adventurer");
+
+// TODO: Implement your server here
 const server = http.createServer((req, res) => {
   // TODO: Implement your routes here
+
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+    // we need to have a route for creating a character
+  if(pathname === "/createCharacter" && req.method ==="POST") {
+    // we need to look for query parameters that establish, the class
+    // Warrior, Mage, or Rogue, Gender (Male, Female, Other), and a fun fact about the character
+    fantasyCharacter = new Character(query.role, query.gender, query.funFact);
+    res.writeHead(201);
+    res.end(`Character created.`);
+  }
+
+  // then we need a post route for confirming the character was created
+  if(pathname === "/confirm" && req.method ==="POST") {
+    res.writeHead(200);
+    res.end(`Character was created.`);
+  }
+
+  // lastly we need a Get route for viewing the character made in the initial post request
+  if(pathname === "/getCharacter" && req.method ==="GET") {
+    res.writeHead(200);
+    res.end(`Character is a ${fantasyCharacter.gender} ${fantasyCharacter.role} who ${fantasyCharacter.funFact}`);
+  }
+  // we will need some sort of data structure for the character and then also an in-memory storage for retrieving whatever we make.
+  // 
 });
 
 server.listen(3000, () => {
--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,79 @@
+/**
+ * Author: William Renard
+ * Date: 4/14/2024
+ * File Name: server.spec.js
+ * Description: the file for all our tests
+ */
+"use strict";
+
 const http = require('http');
 const server = require('../src/server');
 
 // TODO: Implement your tests here
+
+describe("Server", () => {
+  afterAll(() => {
+    server.close();
+  });
+
+  // we need a test for post route for creating character
+  test("responds to post request with character info query parameters", done => {
+    const options = {
+      hostname: 'localhost',
+      port: 3000,
+      path: '/createCharacter?role=Mage&gender=Female&funFact=can%20teleport.',
+      method: 'POST'
+    };
+    const req = http.request(options, res => {
+      let data = '';
+      res.on('data', chunk => {
+      data += chunk;
+    });
+      res.on('end', () => {
+        expect(res.statusCode).toBe(201);
+        expect(data).toBe('Character created.');
+        done();
+      });
+    });
+    req.end();
+  });
+
+  // next we need a test for post route confirming character creation
+
+  test('responds to /confirm POST request', done => {
+    const options = {
+      hostname: 'localhost',
+      port: 3000,
+      path: '/confirm',
+      method: 'POST'
+    };
+    const req = http.request(options, res => {
+      let data = '';
+        res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        expect(res.statusCode).toBe(200);
+        expect(data).toBe('Character was created.');
+        done();
+      });
+    });
+    req.end();
+  });
+
+  // lastly we need a test for a get route that allows us to view the character
+
+  test('responds to /getCharacter GET request', done => {
+    http.get('http://localhost:3000/getCharacter', res => {
+    let data = '';
+      res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        expect(res.statusCode).toBe(200);
+        expect(data).toBe('Character is a Female Mage who can teleport.');
+        done();
+      });
+    });
+  });
+});
