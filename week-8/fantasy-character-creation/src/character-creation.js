f75a70b3e5c0364009a3b150f5db0dbad8bcb2f1
Alex Renard <prioxisfx@gmail.com>
2024-05-12 21:33:36 -0500

    did week 8 assignment

--- week-8/fantasy-character-creation/src/character-creation.js
+++ week-8/fantasy-character-creation/src/character-creation.js
@@ -14,18 +14,44 @@
  * 3. Uncomment the 'module.exports' line under the "For promises" comment.
  */
 
+// chose to do callbacks since it's something I feel like I need more experience with.
 // For callbacks:
-/*
+///*
 const fs = require('fs');
+const { join } = require('path');
 
-function createCharacter(character, callback) {
-  // TODO: Implement this function
+const character = {
+  gender: 'Male',
+  class: 'Warrior',
+  funFact: 'Enjoys cooking in spare time'
+};
+
+const CHARACTER_FILE = join(__dirname, "characters.txt");
+
+function createCharacter(character, callback) { // this is going to be for creating the data
+  const data = character; // get the data for the function
+  fs.writeFile(CHARACTER_FILE, data, (err) => { // write the data to the character_file
+    if (err) { // if FileSystem has an error, this is where it will appear
+      callback(err); // Pass the error to the callback
+    } else {
+      callback(null); // No error, invoke callback without error
+    }
+  });
 }
 
-function getCharacters(callback) {
-  // TODO: Implement this function
+function getCharacters(callback) { // so this is for reading the data
+  fs.readFile(CHARACTER_FILE, { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      const characters = data; // we need to split the characters into individual objects?
+      callback(null, characters); // call callback function with no errors, and with the data we retrieved.
+    }
+  });
 }
-*/
+
+// createCharacter(character);
+//*/
 
 // For promises:
 /*
@@ -42,5 +68,5 @@ async function getCharacters() {
 
 // Uncomment the appropriate exports depending on whether you're using callbacks or promises:
 
-// module.exports = { createCharacter, getCharacters }; // For callbacks
+module.exports = { createCharacter, getCharacters }; // For callbacks
 // module.exports = { createCharacter, getCharacters }; // For promises
--- week-8/fantasy-character-creation/test/character-creation.spec.js
+++ week-8/fantasy-character-creation/test/character-creation.spec.js
@@ -11,7 +11,7 @@
  */
 
 // For callbacks:
-// const fs = require('fs');
+const fs = require('fs');
 
 // For promises:
 // const fs = require('fs').promises;
@@ -20,10 +20,56 @@ describe("Character Creation Module", () => {
   let createCharacter;
   let getCharacters;
 
+  const character = { // going to create character data here as a way to pass it into tests. No sense in rewriting it multiple times
+    gender: 'Male',
+    class: 'Warrior',
+    funFact: 'Enjoys cooking in spare time'
+  };
+
+  // mocks were pulled from the textbook examples
   beforeEach(() => {
     jest.resetModules();
-    // TODO: Set up your mocks here
+    jest.spyOn(fs, 'readFile').mockImplementation((file, options, callback) =>
+      callback(null, {"class": "Warrior", "funFact": "Enjoys cooking in spare time", "gender": "Male"}));
+       jest.spyOn(fs, 'writeFile').mockImplementation((file, data, callback) =>
+      callback(null));
     ({ createCharacter, getCharacters } = require('../src/character-creation'));
+  });
+
+  test("createCharacter writes a new character to the file", (done) => {
+    createCharacter(character, (err) => {
+      expect(err).toBeNull(); // we expect this one to go through accurately without any errors. 
+      done(); 
+    });
+  });
+
+
+  test("getCharacters reads character from the file", (done) => {
+    const expectedCharacter = {   // we expect it to be this data because this is what we have passed in.
+      gender: 'Male',
+      class: 'Warrior',
+      funFact: 'Enjoys cooking in spare time'
+    };
+    getCharacters((err, characters) => {
+      expect(err).toBeNull(); // so when we run this, we expect there to be no errors
+      expect(characters).toEqual(expectedCharacter);
+      done();
+    });
+  });
+
+  // got stuck on this one for awhile but it was because I was only passing one parameter into the 
+  // createCharacter function and it got the function signatures messed up trying to pass the error as character data.
+  test("createCharacter handles errors when writing to file", (done) => {
+    // Mocking fs.writeFile to throw an error
+    fs.writeFile.mockImplementationOnce((filePath, data, callback) => {
+      callback(new Error("File not found"));
+    });
+  
+    createCharacter(character, (err) => {
+      expect(err).not.toBeNull();
+      expect(err.message).toBe("File not found");
+      done();
+    });
   });
 
   // TODO: Write your tests here. You should have at least three tests:
